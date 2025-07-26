fd1dce3f2149a6ebc26bc6a2a80f8951fa01d7b9
phaothu2002x <truongthuan21342@gmail.com>
2025-07-26 15:03:35 +0700

    Neon implement & api routes

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -2,6 +2,7 @@ import CustomButton from '@/components/CustomButton';
 import InputField from '@/components/InputField';
 import OAuth from '@/components/OAuth';
 import { icons, images } from '@/constants';
+import { fetchAPI } from '@/lib/fetch';
 import { useSignUp } from '@clerk/clerk-expo';
 import { Link, router } from 'expo-router';
 import { useState } from 'react';
@@ -55,6 +56,17 @@ const SignUp = () => {
             // If verification was completed, set the session to active
             // and redirect the user
             if (signUpAttempt.status === 'complete') {
+                //create database user
+
+                await fetchAPI('/(api)/user', {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        name: form.name,
+                        email: form.email,
+                        clerkId: signUpAttempt.createdUserId,
+                    }),
+                });
+
                 await setActive({ session: signUpAttempt.createdSessionId });
                 setVerification({ ...verification, state: 'success' });
             } else {
--- app/(root)/(tabs)/home.tsx
+++ app/(root)/(tabs)/home.tsx
@@ -1,15 +1,13 @@
-import { SignedIn, useUser } from '@clerk/clerk-expo';
-import { Text } from 'react-native';
+import { useUser } from '@clerk/clerk-expo';
+import { FlatList } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Page() {
     const { user } = useUser();
 
     return (
-        <SafeAreaView>
-            <SignedIn>
-                <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-            </SignedIn>
+        <SafeAreaView className="bg-general-500">
+            <FlatList />
         </SafeAreaView>
     );
 }
